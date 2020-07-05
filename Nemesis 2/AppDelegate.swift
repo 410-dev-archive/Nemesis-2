89c3cbe43d6727d1bff7794dd5cf54bbad6a95b8
410 <58301450+410-dev@users.noreply.github.com>
2020-07-05 21:29:41 +0900

    Initial Commit

---
+++ Nemesis 2/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Nemesis 2
+//
+//  Created by Hoyoun Song on 2020/07/05.
+//  Copyright © 2020 410. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Nemesis 2/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Nemesis 2
+//
+//  Created by Hoyoun Song on 2020/07/05.
+//  Copyright © 2020 410. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
