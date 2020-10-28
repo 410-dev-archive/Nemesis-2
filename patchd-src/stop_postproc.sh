14d83a171521cdd9e7502ef2b758e89e87124404
410 <58301450+410-dev@users.noreply.github.com>
2020-10-28 17:38:56 +0900

    [PATCHD] Clears remaining cache

--- patchd-src/stop_postproc.sh
+++ patchd-src/stop_postproc.sh
@@ -7,4 +7,12 @@ if [[ -z "$(ps -ax | grep "Relay Classroo[m] ")" ]]; then
 else
 	echo "ERROR-01: Failed Terminating Relay Classroom.app - Detected by: $(ps -ax | grep "Relay Classroo[m]")" > "$2"
 fi
+export output=$(osascript -e 'display dialog "Do you want to clear Lightspeed Relay Classroom statistics? This takes very long time, and will ask for access to data. Please allow access in order to clear cache data." buttons {"No","Yes"} default button 2 with title "Nemesis 2"')
+if [[ "$output" == "button returned:Yes" ]]; then
+	export WebKitDat="$(find /Users -name "com.lightspeed.Classroom" 2>/dev/null)"
+	echo "$WebKitDat" | while read dirAddr
+	do
+		sudo rm -rf "$dirAddr"
+	done
+fi
 exit 0
